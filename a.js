89bae23e9495b6da65c2cb41415b27063b5dac30
“ <>
2021-09-09 09:16:54 +0800

    2021/9/9

--- a.js
+++ a.js
@@ -1,3 +1,28 @@
 var str = "我是王金玉";
 var arr = ["1", "2"];
-var arr1 = ["1", "2", 3];
+var arr1 = ["1", "2"];
+var arr2 = ["1", 2, true]; //tuple类型
+var flag;
+(function (flag) {
+    flag[flag["success"] = 1] = "success";
+    flag[flag["error"] = 0] = "error";
+})(flag || (flag = {}));
+// let f:flag=flag.success
+// function sum(a:number,b:number,...rest:number[]):number{
+//   let sum =a+b
+//   rest.forEach(ele=>sum+=ele)
+//   return sum
+// }
+// console.log(sum(1,2,3,4))
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: string, b: number): string;
+// function add(a: number, b: string): string;
+// function add(a: any, b: any) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+// alert(add("wode",20))
+// 【类】
--- a.ts
+++ a.ts
@@ -6,4 +6,22 @@ enum flag {
   success=1,
   error=0
 }
-let f:flag=flag.success
+// let f:flag=flag.success
+// function sum(a:number,b:number,...rest:number[]):number{
+//   let sum =a+b
+//   rest.forEach(ele=>sum+=ele)
+//   return sum
+// }
+// console.log(sum(1,2,3,4))
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: string, b: number): string;
+// function add(a: number, b: string): string;
+// function add(a: any, b: any) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+// alert(add("wode",20))
+// 【类】
